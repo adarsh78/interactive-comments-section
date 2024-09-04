a01fe8ba569a02254c7222393a57cebc7b9f2e98
Adarsh k <you@example.com>
2024-09-04 18:53:08 +0530

    delete modal position fixed

--- src/Components/DeleteModal.jsx
+++ src/Components/DeleteModal.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 const DeleteModal = ({ handleDelete, setDeleteModalOpen }) => {
-
   const handleDeleteModalClose = () => {
     setDeleteModalOpen(false);
-  }
+  };
 
   return (
     <>
-      <div className="fixed top-[10rem] left-[2.35rem] lg:left-[30rem] z-50">
+      <div className="fixed top-[10rem] left-[2.35rem] lg:left-[37rem] z-50">
         <div className="w-[300px] mx-auto bg-white rounded-md p-5">
           <h2 className="text-[color:hsl(212,24%,26%)] font-medium">
             Delete Comment
@@ -18,14 +17,16 @@ const DeleteModal = ({ handleDelete, setDeleteModalOpen }) => {
             comment and can't be undone.
           </p>
           <div className="flex items-center justify-between gap-4 mt-4">
-            <button 
-            onClick={handleDeleteModalClose}
-            className="text-sm w-[100%] bg-[color:hsl(211,10%,45%)] text-white font-medium py-2 rounded-md cursor-pointer">
+            <button
+              onClick={handleDeleteModalClose}
+              className="text-sm w-[100%] bg-[color:hsl(211,10%,45%)] text-white font-medium py-2 rounded-md cursor-pointer"
+            >
               NO, CANCEL
             </button>
-            <button 
-            onClick={handleDelete}
-            className="text-sm w-[100%] bg-[color:hsl(358,79%,66%)] text-white font-medium py-2 rounded-md cursor-pointer">
+            <button
+              onClick={handleDelete}
+              className="text-sm w-[100%] bg-[color:hsl(358,79%,66%)] text-white font-medium py-2 rounded-md cursor-pointer"
+            >
               YES, DELETE
             </button>
           </div>
